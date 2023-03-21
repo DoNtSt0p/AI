918f7819d1c61c7ec22c58397a238ff0473c841f
DoNtSt0p <Mihail.u2005@mail.ru>
2023-03-21 22:43:12 +0300

    a

--- main.cpp
+++ main.cpp
@@ -1,9 +1,46 @@
 #include <SFML/Graphics.hpp>
+#include <windows.h>
 #include <iostream>
+#include <fstream>
 #include <string>
 
 
-#define vec2f sf::Vector2f
+int rnd(int a, int b)
+{
+	return std::rand() % (b - a + 1) + a;
+}
+
+
+void generate()
+{
+	CreateDirectory("C:\\A\\storage", NULL);
+	
+	std::ofstream source_file("C:\\A\\storage\\source.txt");
+	sf::RenderTexture surface;
+	sf::RectangleShape rect;
+	int BACKGROUND, FILL_COLOR, SIZE, X, Y; // random values
+
+	surface.create(100, 100);
+	for (int i = 0; i < 1000; i++)
+	{
+		BACKGROUND = rnd(159, 255);
+		FILL_COLOR = rnd(0, 95);
+		SIZE = rnd(10, 20) * 2;
+		X = rnd(SIZE, 100 - SIZE);
+		Y = rnd(SIZE, 100 - SIZE);
+		rect.setSize(sf::Vector2f(SIZE, SIZE));
+		rect.setPosition(sf::Vector2f(X, Y));
+		rect.setOrigin(sf::Vector2f(SIZE / 2, SIZE / 2));
+		rect.setFillColor(sf::Color(FILL_COLOR, FILL_COLOR, FILL_COLOR));
+		surface.clear(sf::Color(BACKGROUND, BACKGROUND, BACKGROUND));
+		surface.draw(rect);
+		surface.getTexture().copyToImage().saveToFile("C:\\A\\storage\\" + std::to_string(i) + ".png");
+		source_file << X << " " << Y << " " << SIZE;
+		if (i != 999)
+			source_file << "/n";
+	}
+	source_file.close();
+}
 
 
 class App
@@ -11,8 +48,8 @@ class App
 public:
 	sf::VideoMode mode;
 	sf::RenderWindow window;
-	vec2f pos;
-	sf::Image image;
+	sf::Vector2f pos;
+	sf::RenderTexture texture;
 	App();
 	void run();
 };
@@ -44,6 +81,7 @@ void App::run()
 
 int main()
 {
+	generate();
 	App my_app;
 	my_app.run();
 	return 0;
